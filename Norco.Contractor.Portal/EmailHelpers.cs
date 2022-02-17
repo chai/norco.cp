16dfd1c0af985a64b73091990d7a5093676d0cdd
Chai <chaihongnge@gmail.com>
2022-02-17 11:59:49 +1100

    Started Email template. Configuration in place.

--- Norco.Contractor.Portal/EmailHelpers.cs
+++ Norco.Contractor.Portal/EmailHelpers.cs
@@ -1,13 +1,17 @@
-﻿using MFiles.VAF.Common;
+﻿using DocumentFormat.OpenXml.Packaging;
+using MFiles.VAF.Common;
 using MFilesAPI;
 using MFilesAPI.Extensions.Email;
+using OpenXmlPowerTools;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing.Imaging;
+using System.Xml.Linq;
 
 namespace Norco.Contractor.Portal
 {
@@ -166,6 +170,131 @@ namespace Norco.Contractor.Portal
             return properties;
         }
 
+        private string getEmailContent(string emailName, List<emailInlineImage> images, EnvironmentBase env)
+        {
+            string bodyMessage = "";
+            string filePathname = SysUtils.CreateTempFolder();
+            try
+            {
+                // Create our search builder.
+                var searchBuilder = new MFSearchBuilder(env.Vault);
+                searchBuilder.ObjType((int)MFBuiltInObjectType.MFBuiltInObjectTypeDocument);
+                searchBuilder.Property(MFBuiltInPropertyDef.MFBuiltInPropertyDefNameOrTitle, new TypedValue { Value = emailName });
+                // Execute the search.
 
+                var searchResult = searchBuilder.FindOneEx();
+                if (searchResult != null)
+                {
+                    //https://loicsterckx.wordpress.com/2015/10/30/c-sends-emails-based-on-a-word-template-with-the-net-mail-library/
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+
+
+                        
+                        string fileName = $@"{filePathname}\emailTemplate.docx";
+                        ObjectFiles objFiles = env.Vault.ObjectFileOperations.GetFiles(searchResult.ObjVer);
+                        env.Vault.ObjectFileOperations.DownloadFile(objFiles[1].ID, objFiles[1].Version, fileName);
+
+                     var result=env.Vault.ObjectFileOperations.DownloadFileAsDataURIEx(searchResult.ObjVer, objFiles[1].FileVer);
+
+                        byte[] byteArray = File.ReadAllBytes(fileName);
+                        File.Delete(fileName);
+
+                        memoryStream.Write(byteArray, 0, byteArray.Length);
+                        using (WordprocessingDocument doc = WordprocessingDocument.Open(memoryStream, true))
+                        {
+                            int imageCounter = 0;
+                            HtmlConverterSettings settings = new HtmlConverterSettings()
+                            {
+                                PageTitle = "Letter",
+                                //AdditionalCss = "p { margin-bottom: 20px; }",
+
+                                //used for tag <title> of html page
+                                ImageHandler = imageInfo =>
+                                {
+                                    DirectoryInfo localDirInfo = new DirectoryInfo("img");
+                                    if (!localDirInfo.Exists)
+                                        localDirInfo.Create();
+                                    ++imageCounter;
+                                    string extension = imageInfo.ContentType.Split('/')[1].ToLower();
+                                    ImageFormat imageFormat = null;
+                                    if (extension == "png")
+                                    {
+                                        extension = "gif";
+                                        imageFormat = ImageFormat.Gif;
+                                    }
+                                    else if (extension == "gif")
+                                        imageFormat = ImageFormat.Gif;
+                                    else if (extension == "bmp")
+                                        imageFormat = ImageFormat.Bmp;
+                                    else if (extension == "jpeg")
+                                        imageFormat = ImageFormat.Jpeg;
+                                    else if (extension == "tiff")
+                                    {
+                                        extension = "gif";
+                                        imageFormat = ImageFormat.Gif;
+                                    }
+                                    else if (extension == "x-wmf")
+                                    {
+                                        extension = "wmf";
+                                        imageFormat = ImageFormat.Wmf;
+                                    }
+                                    if (imageFormat == null)
+                                        return null;
+
+                                    string imageFileName = "img/image" +
+                                        imageCounter.ToString() + "." + extension;
+                                    try
+                                    {
+                                        imageInfo.Bitmap.Save(imageFileName, imageFormat);
+                                    }
+                                    catch (System.Runtime.InteropServices.ExternalException) { }
+
+                                    string contentIDHeader = Guid.NewGuid().ToString();
+                                    images.Add(new emailInlineImage { guid = contentIDHeader, filepath = imageFileName, type = @"image/" + extension });
+
+                                    XElement img = new XElement(Xhtml.img,
+                                        new XAttribute(NoNamespace.src, "cid:" + contentIDHeader),
+                                        imageInfo.ImgStyleAttribute,
+                                        imageInfo.AltText != null ?
+                                            new XAttribute(NoNamespace.alt, imageInfo.AltText) : null);
+                                    return img;
+                                }
+                            };
+                            ;
+                            XElement html = HtmlConverter.ConvertToHtml(doc, settings);
+                            XNamespace w = "http://www.w3.org/1999/xhtml";
+                            var bodyContainer = html.Element(w + "body");
+                            XElement myNewElement = new XElement("p", $"%%{env.ObjVerEx.ID}%%");
+                            myNewElement.SetAttributeValue("Style", "font-size: 1px");
+                            bodyContainer.AddAfterSelf(myNewElement);
+                            bodyMessage = bodyContainer.ToStringNewLineOnAttributes();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                //Not loging usual error to Windows Event per M-File cloud requirement
+                //Log SysUtils.ReportErrorToEventLog(e);
+            }
+            finally
+            {
+                if (Directory.Exists(filePathname))
+                {
+                    SysUtils.DeleteFromDisk(filePathname);
+                }
+            }
+            return bodyMessage;
+        }
+
+
+        public class emailInlineImage
+        {
+            public string guid { get; set; }
+            public string type { get; set; }
+            public string filepath { get; set; }
+        }
     }
 }
