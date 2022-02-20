a2f4c3b071cac5b0e6b98ebffdbd22f83079d846
Chai <chaihongnge@gmail.com>
2022-02-21 10:20:56 +1100

    Refactor to remove uneeded code

--- Norco.Contractor.Portal/EmailHelpers.cs
+++ Norco.Contractor.Portal/EmailHelpers.cs
@@ -1,17 +1,9 @@
-﻿using DocumentFormat.OpenXml.Packaging;
-using MFiles.VAF.Common;
+﻿using MFiles.VAF.Common;
 using MFilesAPI;
 using MFilesAPI.Extensions.Email;
-using OpenXmlPowerTools;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing.Imaging;
-using System.Xml.Linq;
 
 namespace Norco.Contractor.Portal
 {
@@ -42,7 +34,7 @@ namespace Norco.Contractor.Portal
                 //var mfPropertyValuesBuilder = GenerateTemplateProperty(env.Vault, env.ObjVerEx);                
                 //try
                 //{
- 
+
 
                 //    var templateObjVerEx = FindTemplate(env.Vault, Configuration.SignatureDetailsClass, Configuration.TemplateName);
                 //    if (templateObjVerEx != null)
@@ -52,7 +44,7 @@ namespace Norco.Contractor.Portal
                 //        List<emailInlineImage> CIDImages = new List<emailInlineImage>();
                 //        CIDImages.Add(new emailInlineImage()
                 //            { filepath = @"C:\norcoemmail\footer.jpeg", guid = env.ObjVerEx.Info.VersionGUID, type = "jpeg" });
-                      
+
                 //        emailBody = getEmailContent(env.Vault, newEmailNotification, "email notification", CIDImages);
 
                 //    }
@@ -66,7 +58,7 @@ namespace Norco.Contractor.Portal
                 //{ }
 
 
-
+                SysUtils.
 
 
                 // Create a message.
@@ -128,32 +120,35 @@ namespace Norco.Contractor.Portal
 
                     //if (expired)
                     //{
-                        //emailMessage.Subject = $"Document {env.ObjVerEx.Title} isseued on {IssueDate} has expired on {ExpiryDate}";
-                        //emailMessage.HtmlBody = $"Our records indicate that Contractor {contractor.Title} from {company.Title} certification for {env.ObjVerEx.Title} has expired on {ExpiryDate}." +
-                        //    $"<br>" +
-                        //    $"The certificate {env.ObjVerEx.Title} is set to expire on the <b>{ExpiryDate}</b>." +
-                        //    $"<br>" +
-                        //    $"Details of the certificate are below: <br> " +
-                        //    $"{certificateProperties}" +
-                        //    $"<br>" +
-                        //    $"Regards," +
-                        //    $"<br>" +
-                        //    $"Norco";
+                    //emailMessage.Subject = $"Document {env.ObjVerEx.Title} isseued on {IssueDate} has expired on {ExpiryDate}";
+                    //emailMessage.HtmlBody = $"Our records indicate that Contractor {contractor.Title} from {company.Title} certification for {env.ObjVerEx.Title} has expired on {ExpiryDate}." +
+                    //    $"<br>" +
+                    //    $"The certificate {env.ObjVerEx.Title} is set to expire on the <b>{ExpiryDate}</b>." +
+                    //    $"<br>" +
+                    //    $"Details of the certificate are below: <br> " +
+                    //    $"{certificateProperties}" +
+                    //    $"<br>" +
+                    //    $"Regards," +
+                    //    $"<br>" +
+                    //    $"Norco";
                     //}
                     //else
                     //{
-                        //    emailMessage.Subject = $"Document {env.ObjVerEx.Title} isseued on {IssueDate} will expire in {NumberOfDays} days, on {ExpiryDate}";
-                        //    emailMessage.HtmlBody = $"Our records indicate that Contractor {contractor.Title} from {company.Title} certification for {env.ObjVerEx.Title} will expired on {ExpiryDate}." +
-                        //                                $"<br>" +
-                        //                                $"The certificate {env.ObjVerEx.Title} is set to expire on the <b>{ExpiryDate}</b>." +
-                        //                                $"<br>" +
-                        //                                $"Details of the certificate are below: <br> " +
-                        //                                $"{certificateProperties}" +
-                        //                                $"<br>" +
-                        //                                $"Regards," +
-                        //                                $"<br>" +
-                        //                                $"Norco";
-                        //}
+                    //    emailMessage.Subject = $"Document {env.ObjVerEx.Title} isseued on {IssueDate} will expire in {NumberOfDays} days, on {ExpiryDate}";
+                    //    emailMessage.HtmlBody = $"Our records indicate that Contractor {contractor.Title} from {company.Title} certification for {env.ObjVerEx.Title} will expired on {ExpiryDate}." +
+                    //                                $"<br>" +
+                    //                                $"The certificate {env.ObjVerEx.Title} is set to expire on the <b>{ExpiryDate}</b>." +
+                    //                                $"<br>" +
+                    //                                $"Details of the certificate are below: <br> " +
+                    //                                $"{certificateProperties}" +
+                    //                                $"<br>" +
+                    //                                $"Regards," +
+                    //                                $"<br>" +
+                    //                                $"Norco";
+                    //}
+                    ResourceManager rm = new ResourceManager("AppResources", typeof(Example).Assembly);
+                    TextFile1Any
+
                        emailMessage.Subject = "test";
                     emailMessage.HtmlBody = env.ObjVerEx.ExpandPlaceholderText( Configuration.EmailTemplate) ;
 
@@ -201,131 +196,5 @@ namespace Norco.Contractor.Portal
             return properties;
         }
 
-        private string getEmailContent(Vault vault, ObjVerEx objVerExTemplate, string emailName, List<emailInlineImage> images)
-        {
-            string bodyMessage = "";
-            string filePathname = SysUtils.CreateTempFolder();
-            try
-            {
-                //// Create our search builder.
-                //var searchBuilder = new MFSearchBuilder(vault);
-                //searchBuilder.ObjType((int)MFBuiltInObjectType.MFBuiltInObjectTypeDocument);
-                //searchBuilder.Property(MFBuiltInPropertyDef.MFBuiltInPropertyDefNameOrTitle, new TypedValue { Value = emailName });
-                //// Execute the search.
-
-                //var objVerExTemplate = searchBuilder.FindOneEx();
-                if (objVerExTemplate != null)
-                {
-                    //https://loicsterckx.wordpress.com/2015/10/30/c-sends-emails-based-on-a-word-template-with-the-net-mail-library/
-                    using (MemoryStream memoryStream = new MemoryStream())
-                    {
-
-
-                        
-                        string fileName = $@"{filePathname}\emailTemplate.docx";
-                        ObjectFiles objFiles = vault.ObjectFileOperations.GetFiles(objVerExTemplate.ObjVer);
-                        vault.ObjectFileOperations.DownloadFile(objFiles[1].ID, objFiles[1].Version, fileName);
-
-                     var result= vault.ObjectFileOperations.DownloadFileAsDataURIEx(objVerExTemplate.ObjVer, objFiles[1].FileVer);
-
-                        byte[] byteArray = File.ReadAllBytes(fileName);
-                        File.Delete(fileName);
-
-                        memoryStream.Write(byteArray, 0, byteArray.Length);
-                        using (WordprocessingDocument doc = WordprocessingDocument.Open(memoryStream, true))
-                        {
-                            int imageCounter = 0;
-                            HtmlConverterSettings settings = new HtmlConverterSettings()
-                            {
-                                PageTitle = "Letter",
-                                //AdditionalCss = "p { margin-bottom: 20px; }",
-
-                                //used for tag <title> of html page
-                                ImageHandler = imageInfo =>
-                                {
-                                    DirectoryInfo localDirInfo = new DirectoryInfo("img");
-                                    if (!localDirInfo.Exists)
-                                        localDirInfo.Create();
-                                    ++imageCounter;
-                                    string extension = imageInfo.ContentType.Split('/')[1].ToLower();
-                                    ImageFormat imageFormat = null;
-                                    if (extension == "png")
-                                    {
-                                        extension = "png";
-                                        imageFormat = ImageFormat.Gif;
-                                    }
-                                    else if (extension == "gif")
-                                        imageFormat = ImageFormat.Gif;
-                                    else if (extension == "bmp")
-                                        imageFormat = ImageFormat.Bmp;
-                                    else if (extension == "jpeg")
-                                        imageFormat = ImageFormat.Jpeg;
-                                    else if (extension == "tiff")
-                                    {
-                                        extension = "tiff";
-                                        imageFormat = ImageFormat.Tiff;
-                                    }
-                                    else if (extension == "x-wmf")
-                                    {
-                                        extension = "wmf";
-                                        imageFormat = ImageFormat.Wmf;
-                                    }
-                                    if (imageFormat == null)
-                                        return null;
-
-                                    string imageFileName = "img/image" +
-                                        imageCounter.ToString() + "." + extension;
-                                    try
-                                    {
-                                        imageInfo.Bitmap.Save(imageFileName, imageFormat);
-                                    }
-                                    catch (System.Runtime.InteropServices.ExternalException) { }
-
-                                    string contentIDHeader = Guid.NewGuid().ToString();
-                                    images.Add(new emailInlineImage { guid = contentIDHeader, filepath = imageFileName, type = @"image/" + extension });
-
-                                    XElement img = new XElement(Xhtml.img,
-                                        new XAttribute(NoNamespace.src, "cid:" + contentIDHeader),
-                                        imageInfo.ImgStyleAttribute,
-                                        imageInfo.AltText != null ?
-                                            new XAttribute(NoNamespace.alt, imageInfo.AltText) : null);
-                                    return img;
-                                }
-                            };
-                            ;
-                            XElement html = HtmlConverter.ConvertToHtml(doc, settings);
-                            XNamespace w = "http://www.w3.org/1999/xhtml";
-                            var bodyContainer = html.Element(w + "body");
-                            XElement myNewElement = new XElement("p", $"%%{objVerExTemplate.ID}%%");
-                            myNewElement.SetAttributeValue("Style", "font-size: 1px");
-                            bodyContainer.AddAfterSelf(myNewElement);
-                            bodyMessage = bodyContainer.ToStringNewLineOnAttributes();
-                        }
-                    }
-                }
-
-            }
-            catch (Exception e)
-            {
-                //Not loging usual error to Windows Event per M-File cloud requirement
-                //Log SysUtils.ReportErrorToEventLog(e);
-            }
-            finally
-            {
-                if (Directory.Exists(filePathname))
-                {
-                    SysUtils.DeleteFromDisk(filePathname);
-                }
-            }
-            return bodyMessage;
-        }
-
-
-        public class emailInlineImage
-        {
-            public string guid { get; set; }
-            public string type { get; set; }
-            public string filepath { get; set; }
-        }
     }
 }
